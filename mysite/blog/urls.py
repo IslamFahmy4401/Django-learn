f3695410eab22655cbcc79e391dba5fc954ee483
Islam Fahmy <islam4401@gmail.com>
2023-11-24 17:56:02 +0200

    Building Class Based Views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.home),
     path("about/", views.about),
     path("post_list/", views.post_list, name='post_list'),
+    # path("post_list/", views.PostListView.as_view(), name='post_list'),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/",
          views.post_details, name='post_details')
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from . models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -30,6 +31,12 @@ def post_list(request):
         posts = paginator.page(1)
 
     return render(request, 'post_list.html', {'posts': posts})
+
+# class PostListView(ListView): # Building Class Based Views
+#     model = Post
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = "post_list.html"
 
 
 def post_details(request, year, month, day, post):
